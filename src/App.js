8dfe24639abe518a84ddd51544b85c48a5c45aea
ninaadak03 <ninkaller03@gmail.com>
2024-09-18 23:54:39 +0530

    Initial Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [name,setName] = useState('');
+  const [datetime,setDatetime] = useState('');
+  const [description,setDescription] = useState('');
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL+'/transactions';
+    const response = await fetch(url);
+    return await response.json();
+  }
+  
+  function addNewTransaction(ev) {
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL+'/transaction';
+    const price = name.split(' ')[0];
+    fetch(url, {
+      method:'POST',
+      headers: {'Content-type':'application/json'},
+      body: JSON.stringify({
+        name:name.substring(price.length+1),
+        description,
+        price,
+        datetime,
+      })
+    }).then(response => {
+      response.json().then(json => {
+        setTransactions([...transactions, json]);
+        setName('');
+        setDatetime('');
+        setDescription('');
+        console.log('result',json);
+      });
+    });
+  }
+
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.price;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Rs. {balance}</h1>
+      <form onSubmit={addNewTransaction}>
+        <div className="basic">
+          <input type="text" 
+          value={name}
+          onChange={ev => setName(ev.target.value)}
+          placeholder={'Enter "+/- amount name"'}/>
+          <input value={datetime}
+          onChange={ev => setDatetime(ev.target.value)}
+          type="date"/>
+        </div>
+        <div className="description">
+          <input type="text" value={description}
+          onChange={ev => setDescription(ev.target.value)}
+          placeholder={'Enter Description'}/>
+        </div>
+        <div className="button-wrapper">
+    <button type="submit">Add New Transaction</button>
+  </div>
+      </form>
+      <div className="transactions">
+        {transactions.length > 0 && transactions.map(transaction => (
+          <div className="transaction">
+          <div className="left">
+            <div className="name">{transaction.name}</div>
+            <div className="description">{transaction.description}</div>
+          </div>
+          <div className="right">
+            {console.log(transaction.price)}
+            <div className={"price " +(transaction.price<0?'red':'gre')}>
+              {transaction.price}
+            </div>
+            <div className="datetime">
+              {new Date(transaction.datetime).toLocaleDateString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              })}
+            </div>
+          </div>
+        </div>
+        ))}
+        
+      </div>
+    </main>
   );
 }
 
